3d08833492aaee7a8a25e47a9c5404b3e0fd12b4
PhilipGeil <pgj.philip@gmail.com>
2020-11-11 12:17:56 +0100

    First commit

---
+++ WebPage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RequestManager r = new RequestManager(new WebRequestDal());
+            Console.WriteLine("Indtast url");
+            Console.WriteLine(r.GetData("https://" + Console.ReadLine()));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WebPage/WebRequestDal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPage
+{
+    class WebRequestDal : IDataLayer
+    {
+        public string GetData(string path)
+        {
+            WebRequest webRequest = WebRequest.Create(path);
+            WebResponse webResponse = webRequest.GetResponse();
+            Stream s = webResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(s);
+            return streamReader.ReadToEnd();
+        }
+    }
+}
